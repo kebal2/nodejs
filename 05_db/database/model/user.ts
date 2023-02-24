06df489da774650beba27f37b023c57bda0e733f
Bálint Kelemen <kebal84@gmail.com>
2023-02-24 20:11:50 +0100

    user constructor mapping

--- 05_db/database/model/user.ts
+++ 05_db/database/model/user.ts
@@ -1,20 +1,29 @@
-import { Entity, Index, PrimaryKey, Property } from '@mikro-orm/core';
-import { IUser } from "../document/i.user";
+import {Entity, Index, PrimaryKey, Property, Unique} from '@mikro-orm/core';
+import {IUser} from "../document/i.user";
 
 @Entity()
 export class User implements IUser {
 
-    @PrimaryKey()
-    @Index()
-    id!: number;
+  @PrimaryKey()
+  @Index()
+  id!: number;
 
-    @Property()
-    name!: string;
+  @Property()
+  name!: string;
 
-    @Property()
-    email!: string;
+  @Property()
+  @Unique()
+  email!: string;
 
-    @Property({ nullable: true })
-    avatar: string | undefined;
+  @Property({nullable: true})
+  avatar: string | undefined;
+
+  constructor(u?: IUser) {
+    if(u){
+      this.avatar = u.avatar;
+      this.email = u.email;
+      this.name = u.name;
+    }
+  }
 
 }
