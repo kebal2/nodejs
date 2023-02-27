3d44fb446f55b1ca035be9572cd03ce7dc6ea31f
Bálint Kelemen <kebal84@gmail.com>
2023-02-27 10:42:32 +0100

    WIP: repository factory

--- 05_db/app.ts
+++ 05_db/app.ts
@@ -1,15 +1,17 @@
-import { Application } from './app/application';
+import * as fs from "fs";
 import dotenv from 'dotenv';
 import express from "express";
-import { IApplicationConfiguration } from "./app/configuration";
-import * as fs from "fs";
-import { UserController } from "./app/controllers/user.conrtoller";
-import { MikroORM } from "@mikro-orm/core";
-import { EntityManager } from "@mikro-orm/core/EntityManager";
-import { SqliteDriver } from '@mikro-orm/sqlite';
-import { SqliteUserRepository } from './database/repositories/user.repository.sqlite';
-import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
+import {Application} from './app/application';
+import {IApplicationConfiguration} from "./app/configuration";
+import {MariaDbDriver} from "@mikro-orm/mariadb";
+import {MikroORM} from "@mikro-orm/core";
+import {MongoDriver} from "@mikro-orm/mongodb";
+import {SqlHighlighter} from '@mikro-orm/sql-highlighter';
+import {SqliteDriver} from '@mikro-orm/sqlite';
+import {SqliteUserRepository} from './database/repositories/user.repository.sqlite';
+import {UserController} from "./app/controllers/user.conrtoller";
 import {updateSchema} from './database/schema.init';
+
 function LoadConfig(): Promise<IApplicationConfiguration> {
 
   dotenv.config();
@@ -42,30 +44,44 @@ function LoadConfig(): Promise<IApplicationConfiguration> {
 
 }
 
-LoadConfig().then(async (conf) => {
+async function dbFact(microOrmSettings: any) {
+  switch (microOrmSettings.type) {
+    case "sqlite":
+      return await MikroORM.init<SqliteDriver>(microOrmSettings);
+    case "mariadb":
+      return await MikroORM.init<MariaDbDriver>(microOrmSettings);
+    case "mongodb":
+      return await MikroORM.init<MongoDriver>(microOrmSettings);
+    // default:
+    //   new Error("Unhanded db type!");
+  }
+}
 
-  let em: EntityManager;
+LoadConfig()
+  .then(async (conf) => {
 
-  let microOrmSettings: any = {
-    entities: ['./dist/database/model'], // path to our JS entities (dist), relative to `baseDir`
-    entitiesTs: ['./database/model'], // path to our TS entities (src), relative to `baseDir`
-    dbName: 'test.db',
-    type: conf.dbtype.toLowerCase(),
-    highlighter: new SqlHighlighter(),
-    debug: true,
-  };
+    // const microOrmSettings: MikroORMOptions = {
+    const microOrmSettings: any = {
+      entities: ['./dist/database/model'], // path to our JS entities (dist), relative to `baseDir`
+      entitiesTs: ['./database/model'], // path to our TS entities (src), relative to `baseDir`
+      dbName: 'test.db',
+      type: conf.dbtype.toLowerCase(),
+      highlighter: new SqlHighlighter(),
+      debug: true,
+    };
 
-  await updateSchema(microOrmSettings);
+    await updateSchema(microOrmSettings);
 
-  const orm = await MikroORM.init<SqliteDriver>(microOrmSettings);
-  const router = express.Router();
-  new Application(
-    conf,
-    [
-      new UserController(conf, new SqliteUserRepository(orm), router),
-    ],
-    '/api'
-  ).Start();
+    const orm = await dbFact(microOrmSettings);
 
-});
+    const router = express.Router();
+    new Application(
+      conf,
+      [
+        new UserController(conf, new SqliteUserRepository(orm as MikroORM<SqliteDriver>), router),
+      ],
+      '/api'
+    ).Start();
+
+  });
 
