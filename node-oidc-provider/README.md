2c1ecb8fb6113da5cde944670d70b7a4d2ebe94b
Deák Tamás <dk.tamas@gmail.com>
2023-04-27 09:33:24 +0200

    add: node-oidc-provider

---
+++ node-oidc-provider/README.md
@@ -0,0 +1,5 @@
+# node-oidc-provider
+
+https://github.com/panva/node-oidc-provider alapuló provider. Default 3000-es porton fut.
+teszt CLIENT_ID: CLIENT_ID
+teszt CLIENT_SECRET: CLIENT_SECRET
---
+++ node-oidc-provider/index.js
@@ -0,0 +1,16 @@
+import Provider from 'oidc-provider';
+
+const configuration = {
+  // refer to the documentation for other available configuration
+  clients: [{
+    client_id: 'CLIENT_ID',
+    client_secret: 'CLIENT_SECRET',
+    redirect_uris: ['http://localhost:3001/callback'],
+  }],
+};
+
+const oidc = new Provider('http://localhost:3000', configuration);
+
+oidc.listen(3000, () => {
+  console.log('oidc-provider listening on port 3000, check http://localhost:3000/.well-known/openid-configuration');
+});
